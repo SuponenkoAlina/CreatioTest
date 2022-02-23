452a7f985f1e9b7dbdbed008af56a000dfbd2c70
SuponenkoAlina <suponenkoalina@gmail.com>
2022-02-23 12:26:35 +0300

    SqlOmneemo

---
+++ Custom/Schemas/ContactPageV2/ContactPageV2.js
@@ -0,0 +1,186 @@
+define("ContactPageV2", [], function() {
+	return {
+		entitySchemaName: "Contact",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "LOOKUPf4818910-ce00-4664-a4aa-971aeb8b10c7",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 6,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "OmnLookupOffice",
+					"enabled": true,
+					"contentType": 3
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "merge",
+				"name": "Dear",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2
+					}
+				}
+			},
+			{
+				"operation": "move",
+				"name": "Dear",
+				"parentName": "ContactGeneralInfoBlock",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "merge",
+				"name": "Age",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 3
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "ContactPageServiceTab",
+				"values": {
+					"order": 2
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "CommunicationChannelsTab",
+				"values": {
+					"order": 6
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DoNotUseEmail",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DoNotUseSms",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0
+					}
+				}
+			},
+			{
+				"operation": "move",
+				"name": "DoNotUseSms",
+				"parentName": "CommunicationChannelsContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "merge",
+				"name": "DoNotUseCall",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "DoNotUseMail",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "JobTabContainer",
+				"values": {
+					"order": 7
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "HistoryTab",
+				"values": {
+					"order": 8
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "NotesAndFilesTab",
+				"values": {
+					"order": 9
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "TrackingEventsTab",
+				"values": {
+					"order": 5
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "WrongBrowserInfoDescription",
+				"values": {
+					"layout": {
+						"colSpan": 20,
+						"rowSpan": 5,
+						"column": 3,
+						"row": 7
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "ESNTab",
+				"values": {
+					"order": 10
+				}
+			},
+			{
+				"operation": "move",
+				"name": "Language",
+				"parentName": "ContactGeneralInfoBlock",
+				"propertyName": "items",
+				"index": 5
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
---
+++ Custom/Schemas/OmnOffice1b9a3518Section/OmnOffice1b9a3518Section.js
@@ -0,0 +1,8 @@
+define("OmnOffice1b9a3518Section", [], function() {
+	return {
+		entitySchemaName: "OmnOffice",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
